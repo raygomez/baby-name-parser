d7d6ff8f6e33af52a6773a15375386a5b17f97d4
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-11 13:07:00 +0800

    parse all files.

--- parser.py
+++ parser.py
@@ -18,25 +18,44 @@ from os import listdir
 from os.path import isfile, join
 
 files = [f for f in listdir(RESOURCES) if isfile(join(RESOURCES,f))]
+size = len(files)
+males = {}
+females = {}
 
-males = []
-females = []
 
-soup = BeautifulSoup(open('babynames/baby1990.html'), 'lxml')
-print(soup.h3.get_text())
-table = soup.find_all('table')[1]
-for tr in table.find_all('tr'):
-    if tr.has_attr('align') and tr['align'] == 'right':
-        td = tr.find_all('td')
-        male = {'name': td[1].string, 'rank' : [None]}
-        index = td[0].string
-        male['rank'] = [index]
-        males.append(male)
+for index, f in enumerate(files):
 
-        female = {'name': td[2].string, 'rank' : [None]}
-        index = td[0].string
-        female['rank'] = [index]
-        females.append(female)
+    #print(index, f)
+    soup = BeautifulSoup(open(join(RESOURCES,f)), 'lxml')
 
-print(males)
+    try:
+        pass
+        #print(soup.h3.get_text())
+    except AttributeError:
+        pass
+        #print(soup.h2.get_text())
+
+    table = soup.find_all('table')[1]
+    for tr in table.find_all('tr'):
+        if tr.has_attr('align') and tr['align'] == 'right':
+            td = tr.find_all('td')
+            rank = td[0].string
+
+            name = td[1].string
+            if name not in males:
+                male = {'name': name, 'rank' : [None]*size}
+                males[name] = male
+            males[name]['rank'][index] = rank
+
+            name = td[2].string
+            if name not in females:
+                female = {'name': name, 'rank' : [None]*size}
+                females[name] = female
+            females[name]['rank'][index] = rank
+
+# with open('male.csv', 'w') as f:
+#     for name in sorted(males.iteritems()):
+#         f.write('{},{}'.format(name['name'], ','.join(name['rank'])))
+#
+
 print(females)
