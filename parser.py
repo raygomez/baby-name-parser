94f41297cfb0be48f89e9f56322956dbbe351983
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-11 22:29:33 +0800

    refactor code

--- parser.py
+++ parser.py
@@ -18,6 +18,14 @@ name_regex = re.compile('<tr align="right"><td>(\w+)</td><td>(\w+)</td><td>(\w+)
 header_regex = re.compile('<h3 align="center">Popularity in (\d+)</h3>')
 other_header_regex = re.compile('<h2>Popularity in (\d+)</h2>')
 
+
+def add_name_to_list(list_of_names, name, rank, i):
+    if name not in list_of_names:
+        item = {'name': name, 'rank': [''] * size}
+        list_of_names[name] = item
+    list_of_names[name]['rank'][i] = rank
+
+
 for index, f in enumerate(files):
     with open(f) as f:
         text = f.read()
@@ -31,24 +39,14 @@ for index, f in enumerate(files):
         for results in re.findall(name_regex, text):
             rank = results[0]
 
-            name = results[1]
-            if name not in males:
-                male = {'name': name, 'rank' : ['']*size}
-                males[name] = male
-            males[name]['rank'][index] = rank
+            add_name_to_list(males, results[1], rank, index)
+            add_name_to_list(females, results[2], rank, index)
 
-            name = results[2]
-            if name not in females:
-                female = {'name': name, 'rank' : ['']*size}
-                females[name] = female
-            females[name]['rank'][index] = rank
+def write_names(filename, list_of_names):
+    with open(filename, 'w') as f:
+        f.write("{},{}\n".format('Name', ','.join(years)))
+        for name in sorted(list_of_names.iteritems()):
+            f.write('{},{}\n'.format(name[1]['name'], ','.join(name[1]['rank'])))
 
-with open('male.csv', 'w') as f:
-    f.write("{},{}\n".format('Name', ','.join(years)))
-    for name in sorted(males.iteritems()):
-        f.write('{},{}\n'.format(name[1]['name'], ','.join(name[1]['rank'])))
-
-with open('female.csv', 'w') as f:
-    f.write("{},{}\n".format('Name', ','.join(years)))
-    for name in sorted(females.iteritems()):
-        f.write('{},{}\n'.format(name[1]['name'], ','.join(name[1]['rank'])))
+write_names('male.csv', males)
+write_names('female.csv', females)
